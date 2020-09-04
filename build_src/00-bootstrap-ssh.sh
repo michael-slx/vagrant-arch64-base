13b5a30e528507368982c4470b47865ba1efe375
Michael Schantl <michael@schantl-lx.at>
2020-09-04 09:01:38 +0200

    Don't enable ssh service on live system
    Starting is enough

--- build_src/00-bootstrap-ssh.sh
+++ build_src/00-bootstrap-ssh.sh
@@ -2,4 +2,4 @@
 
 sed -i '/^#PermitEmptyPasswords/ s/^#//' /etc/ssh/sshd_config
 sed -i '/^PermitEmptyPasswords/ s/no/yes/' /etc/ssh/sshd_config
-systemctl enable --now sshd dhcpcd
+systemctl start sshd dhcpcd
