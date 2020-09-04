340e3b83f19ca24165270f053be760e66bcefb6d
Michael Schantl <michael@schantl-lx.at>
2020-09-04 09:04:29 +0200

    Don't run reflector on the live system
    The live system now includes a service unit doing this automatically.

--- build_src/install-scripts/20-pacman.sh
+++ build_src/install-scripts/20-pacman.sh
@@ -2,8 +2,5 @@
 
 set -euo pipefail
 
-echo 'Fetching up-to-date mirrorlist & performing system update'
-pacman -Syy --noconfirm reflector
-reflector --verbose --latest 10 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist.bak
-mv /etc/pacman.d/mirrorlist.bak /etc/pacman.d/mirrorlist
+echo 'Performing package list update'
 pacman -Syy --noconfirm
