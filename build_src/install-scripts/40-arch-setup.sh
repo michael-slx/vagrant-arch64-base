82e01e265f5106216fcca5ce462ee16742e41ee8
Michael Schantl <michael@schantl-lx.at>
2020-12-04 15:18:47 +0100

    Include man & info

--- build_src/install-scripts/40-arch-setup.sh
+++ build_src/install-scripts/40-arch-setup.sh
@@ -6,7 +6,7 @@ echo "Installing packages"
 SYSTEM="base linux e2fsprogs dosfstools systemd-resolvconf openssh reflector"
 GUEST_UTILS="virtualbox-guest-utils-nox"
 UCODE="amd-ucode intel-ucode"
-UTILS="neovim wget curl sudo"
+UTILS="neovim wget curl sudo man-db man-pages texinfo"
 SHELL="zsh grml-zsh-config"
 PACKAGES="$SYSTEM $GUEST_UTILS $UCODE $UTILS $SHELL"
 pacstrap /mnt $PACKAGES
