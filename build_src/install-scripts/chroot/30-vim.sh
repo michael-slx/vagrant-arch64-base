e8aced7e4b84120a5d38577469b226df2e0a3a94
Michael Schantl <michael@schantl-lx.at>
2021-02-06 20:31:36 +0100

    Switch to a more traditional neovim config
    No line marking & no line numbers

--- build_src/install-scripts/chroot/30-vim.sh
+++ build_src/install-scripts/chroot/30-vim.sh
@@ -22,8 +22,6 @@ set expandtab                           " Converts tabs to spaces
 set smartindent                         " Makes indenting smart
 set autoindent                          " Good auto indent
 set laststatus=0                        " Always display the status line
-set number                              " Line numbers
-set cursorline                          " Enable highlighting of the current line
 set background=dark                     " tell vim what the background color looks like
 set nobackup                            " This is recommended by coc
 set nowritebackup                       " This is recommended by coc
