da3cfbb97e48a8ce62c664e21bb4e1120f807a1a
Michael Schantl <michael@schantl-lx.at>
2021-05-01 15:50:51 +0200

    Remove ucode updates
    Not required in a VM

--- build_src/install-scripts/40-arch-setup.sh
+++ build_src/install-scripts/40-arch-setup.sh
@@ -5,10 +5,9 @@ set -euo pipefail
 echo "Installing packages"
 SYSTEM="base linux e2fsprogs dosfstools systemd-resolvconf openssh reflector"
 GUEST_UTILS="virtualbox-guest-utils-nox"
-UCODE="amd-ucode intel-ucode"
 UTILS="neovim wget curl sudo man-db man-pages texinfo"
 SHELL="zsh grml-zsh-config"
-PACKAGES="$SYSTEM $GUEST_UTILS $UCODE $UTILS $SHELL"
+PACKAGES="$SYSTEM $GUEST_UTILS $UTILS $SHELL"
 pacstrap /mnt $PACKAGES
 
 echo "Generating fstab"
