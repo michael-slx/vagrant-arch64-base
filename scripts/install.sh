397475b78f64f1aebae70ca648428c1536449e45
Michael Schantl <floss@schantl-lx.at>
2022-04-03 15:15:57 +0200

    Move setup files to /setup

--- scripts/install.sh
+++ scripts/install.sh
@@ -39,15 +39,16 @@ pacstrap /mnt ${PACKAGES[@]}
 genfstab -L /mnt >> /mnt/etc/fstab
 sed -i -E '/\/boot/ s/(rw,\S*)/\1,noauto,x-systemd.automount/' /mnt/etc/fstab
 
-mkdir -p /mnt/root/setup/scripts
-cp -R $SCRIPT_DIR/* /mnt/root/setup/scripts
+mkdir -p /mnt/setup/scripts
+cp -R $SCRIPT_DIR/* /mnt/setup/scripts
+chmod -R +x /mnt/setup
 
-mkdir -p /mnt/root/setup/files
-cp -R $FILES_DIR/* /mnt/root/setup/files
+mkdir -p /mnt/setup/files
+cp -R $FILES_DIR/* /mnt/setup/files
 
-arch-chroot /mnt env "FILES_DIR=/root/setup/files" \
-                     "SCRIPT_DIR=/root/setup/scripts" \
-                     /root/setup/scripts/chroot.sh
+arch-chroot /mnt env "FILES_DIR=/setup/files" \
+                     "SCRIPT_DIR=/setup/scripts" \
+                     /setup/scripts/chroot.sh
 
 ln -sf "/run/systemd/resolve/stub-resolv.conf" /mnt/etc/resolv.conf
 
@@ -58,7 +59,7 @@ efibootmgr \
     --unicode 'root=LABEL=ROOT resume=LABEL=SWAP rootflags=rw,relatime initrd=\initramfs-linux.img add_efi_memmap random.trust_cpu=on rng_core.default_quality=1000 nomodeset nowatchdog mitigations=off quiet loglevel=3 rd.systemd.show_status=auto rd.udev.log_priority=3' \
     --verbose
 
-rm -R /mnt/root/setup
+rm -R /mnt/setup
 rm -f /mnt/etc/machine-id
 
 sync
